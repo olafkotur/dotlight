62a65c6dc3938e5e7507502aec2f3d4219964d3f
olafkotur <olafkotur97@gmail.com>
2021-12-02 20:23:51 +0000

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { TwitterService } from './services/twitter';
+import _ from 'lodash';
 
 const main = async () => {
   const data = await TwitterService.fetchSearch('casting call');
-  console.log(data);
+
+  const filtered = data.filter((v) => {
+    const keywords: string[] = ['audition', 'paid', '$', '£', 'female', 'apply', 'application'];
+    for (const word of keywords) {
+      if (v.text.includes(word)) {
+        return true;
+      }
+    }
+  });
+
+  // console.log(data);
+  console.log(filtered);
 };
 
 main();
